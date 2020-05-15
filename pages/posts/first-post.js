34e95cea58338996046ec88d7a362bd16fbee192
teal.yao <teal.yao@corp.to8to.com>
2020-05-15 16:05:14 +0800

    feat: add reactive data

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,25 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '../../components/layout'
+import React from 'react'
 
-export default function FirstPost() {
+export default function FirstPost({name}) {
+  let [age, setAge] = React.useState(0)
+  React.useEffect(() => {
+    setTimeout(() => {
+      setAge(30)
+      document.title = '30'
+    }, 1000)
+  }, [])
+
   return (
     <Layout>
       <Head>
         <title>First Post</title>
       </Head>
       <h1>First Post</h1>
+      <p>name: {name}</p>
+      <p>age: {age}</p>
       <h2>
         <Link href="/">
           <a>Back to home</a>
@@ -16,4 +27,12 @@ export default function FirstPost() {
       </h2>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      name: 'teal'
+    }
+  }
 }
